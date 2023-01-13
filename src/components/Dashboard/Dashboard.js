29fbb453e23cf190961ddf2b10ae0cc873f53903
Ferran <ferran@protonmail.ch>
2023-01-13 12:12:10 +0100

    Create views

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DashboardView() {
+    return (
+        <h1>Dashboard View</h1>
+    );
+}
---
+++ src/components/FraudAnalysis/FraudAnalysis.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function FraudAnalysisView() {
+    return (
+        <h1>Fraud Analysis View</h1>
+    );
+}
