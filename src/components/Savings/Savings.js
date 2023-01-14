a6b9d89b36fdbc0e544d740457f26d2b8beca74f
Ferran <ferran@protonmail.ch>
2023-01-14 12:29:01 +0100

    Style savings component

--- src/components/Savings/Savings.js
+++ src/components/Savings/Savings.js
@@ -1,51 +1,67 @@
 // React
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import "./Savings.scss"
+import "./Savings.scss";
 
 function SavingsComponent() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await fetch(
-                    `https://us-central1-opticks-test.cloudfunctions.net/savings`
-                );
-                if (!response.ok) {
-                    throw new Error(
-                        `This is an HTTP error: The status is ${response.status}`
-                    );
-                }
-                let actualData = await response.json();
-                setData(actualData);
-                setError(null);
-            } catch (err) {
-                setError(err.message);
-                setData(null);
-            } finally {
-                setLoading(false);
-            }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://us-central1-opticks-test.cloudfunctions.net/savings`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
         }
-        getData()
-    }, [])
+        let actualData = await response.json();
+        setData(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
 
-    return (
-        <div className="savings">
-            <h1>Savings Component</h1>
-            {loading && <div>A moment please...</div>}
-            {error && (
-                <div>{`There is a problem fetching the post data - ${error}`}</div>
-            )}
-            {data && <div>
-                <p>total: {data.total}</p>
-                <p>last30days: {data.last30days}</p>
-                <p>next30days: {data.next30days}</p>
-            </div>}
-        </div>
-    )
+  return (
+    <div className="savings">
+      <h3>Savings</h3>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      {data && (
+        <main>
+          <section>
+            <span>Total saved</span>
+            <br />
+            {data.total}
+          </section>
+          <section>
+            <aside>
+              <span>Last 30 days</span>
+              <br />
+              {data.last30days}
+            </aside>
+            <aside>
+              <span>Next 30 days projection</span>
+              <br />
+              {data.next30days}
+            </aside>
+          </section>
+        </main>
+      )}
+    </div>
+  );
 }
 
 export default SavingsComponent;
