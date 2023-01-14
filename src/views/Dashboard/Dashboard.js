0043f8020d149b375d8d6a0f2ab13137ac858f7f
Ferran <ferran@protonmail.ch>
2023-01-14 22:02:52 +0100

    Add pie chart to dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -8,11 +8,21 @@ import Widget from "../../components/Widget/Widget";
 import Range from "../../components/Range/Range";
 import CentralContent from "../../components/CentralContent/CentralContent";
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 function DashboardView() {
   const [threatsData, setThreatsData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [range, setRange] = useState("today");
+
+  const [sumInvalidRisk, setSumInvalidRisk] = useState(null);
+  const [sumSuspiciousRisk, setSumSuspiciousRisk] = useState(null);
+  const [sumLegitimateRisk, setSumLegitimateRisk] = useState(null);
+  const [sumTotalRisk, setSumTotalRisk] = useState(null);
 
   // Stats
   useEffect(() => {
@@ -27,7 +37,29 @@ function DashboardView() {
           );
         }
         let actualData = await response.json();
+        // if (range === 'last_7_days' || range === 'last_30_days') {
+        //   console.log(actualData);
+
+        //   actualData.forEach(element => {
+        //     console.log(element.risk.invalid);
+        //     setSumRisk(element.risk.invalid);
+        //   });
+
+        //   // actualData.map(({ risk }) => (
+        //   //   console.log(risk.invalid),
+        //   //   console.log(risk.suspicious),
+        //   //   console.log(risk.legitimate)
+        //   // ));
+
+        //   setThreatsData(actualData);
+        // } else {
+        //   setThreatsData(actualData);
+        // }
         setThreatsData(actualData);
+        setSumInvalidRisk(actualData[0].risk.invalid);
+        setSumSuspiciousRisk(actualData[0].risk.suspicious);
+        setSumLegitimateRisk(actualData[0].risk.legitimate);
+        setSumTotalRisk(sumInvalidRisk + sumSuspiciousRisk + sumLegitimateRisk);
         setError(null);
       } catch (err) {
         setError(err.message);
@@ -39,6 +71,19 @@ function DashboardView() {
     getData();
   }, [range]);
 
+  console.log(threatsData);
+  console.log(sumTotalRisk);
+
+  const data = {
+    labels: ["Invalid", "Suspicious", "Legitimate"],
+    datasets: [
+      {
+        data: [sumInvalidRisk, sumSuspiciousRisk, sumLegitimateRisk],
+        backgroundColor: ["#F05641", "#FEB031", "#25D184"],
+      },
+    ],
+  };
+
   return (
     <div className="dashboard">
       <Range range={range} setRange={setRange} />
@@ -46,22 +91,14 @@ function DashboardView() {
         <Widget title={"Invalid Traffic over time"} />
         <Widget title={"Traffic Veracity"}>
           <p>{range}</p>
-          {loading && <div>A moment please...</div>}
-          {error && (
-            <div>{`There is a problem fetching the post data - ${error}`}</div>
-          )}
-          {threatsData &&
-            threatsData.map(({ day, risk, total }) => (
-              <div key={day}>
-                <p>
-                  <b>day:</b> {day}
-                </p>
-                <p>invalid: {risk.invalid}</p>
-                <p>suspicious: {risk.suspicious}</p>
-                <p>legitimate: {risk.legitimate}</p>
-                <h3>total: {total}</h3>
-              </div>
-            ))}
+          <div className="dashboard__widgets--traffic-veracity">
+            <aside>
+              <Doughnut data={data} />
+            </aside>
+            <aside>
+              <p>test</p>
+            </aside>
+          </div>
         </Widget>
       </div>
       <CentralContent title={"Other Widgets"} />
