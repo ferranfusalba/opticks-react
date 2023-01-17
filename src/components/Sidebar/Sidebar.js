f0209e647d603624f80b94d82e2bd7dc01f3de82
Ferran <ferran@protonmail.ch>
2023-01-17 12:04:57 +0100

    Update sidebar with current active route

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,32 +1,45 @@
 // React
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
 
 // Assets
-import dashboard from "../../assets/icons/dashboard.svg"
-import fraud from "../../assets/icons/fraud.svg"
+import dashboard from "../../assets/icons/dashboard.svg";
+import fraud from "../../assets/icons/fraud.svg";
 
 // Component
-import './Sidebar.scss';
+import "./Sidebar.scss";
 
-function SidebarElement({ src, name, route }) {
-    return (
-        <div className="element">
-            <Link to={route}>
-                <img src={src} alt={name + 'Icon'} />
-                <p>{name}</p>
-            </Link>
-        </div>
-    )
+function SidebarElement({ src, name, route, routeStatus }) {
+  return (
+    <div className={"element element--" + routeStatus}>
+      <div className="element--statusbar"></div>
+      <Link to={route}>
+        <img src={src} alt={name + "Icon"} />
+        <p>{name}</p>
+      </Link>
+    </div>
+  );
 }
 
 function SidebarComponent() {
-    return (
-        <div className="sidebar">
-            <p className="sidebar--title">INTELLIGENCE</p>
-            <SidebarElement src={dashboard} name={'Dashboard'} route={'/dashboard'} />
-            <SidebarElement src={fraud} name={'Fraud Analysis'} route={'/fraud-analysis'} />
-        </div>
-    )
+  const location = useLocation();
+
+  return (
+    <div className="sidebar">
+      <p className="sidebar--title">INTELLIGENCE</p>
+      <SidebarElement
+        src={dashboard}
+        name={"Dashboard"}
+        route={"/dashboard"}
+        routeStatus={location.pathname === '/dashboard' ? 'active' : 'unactive'}
+      />
+      <SidebarElement
+        src={fraud}
+        name={"Fraud Analysis"}
+        route={"/fraud-analysis"}
+        routeStatus={location.pathname === '/fraud-analysis' ? 'active' : 'unactive'}
+      />
+    </div>
+  );
 }
 
 export default SidebarComponent;
